a288b711540adc7992a028b4fb043f21954727c0
Mukund Shankar <mukunds@nexusvulcan00.umiacs.umd.edu>
2024-05-03 10:58:53 -0400

    guidance stuff (line 468 pipeline)

--- models/aerialbooth_noisy/pipeline.py
+++ models/aerialbooth_noisy/pipeline.py
@@ -465,7 +465,7 @@ class ImagicStableDiffusionPipeline(DiffusionPipeline):
 
             if guidance_scale > 1.0:
                 # noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                noise_pred = noise_pred.chunk(2)
+                noise_pred = guidance_scale * noise_pred.chunk(2)
                 # noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
             latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
